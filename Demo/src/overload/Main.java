51f59a13a6a373a639332a6ef128e5b73a6e9d99
Sick-CS <malhiajay76@Gmail.com>
2022-03-22 14:05:53 +0530

    This is my First Commit

---
+++ Demo/src/overload/Main.java
@@ -0,0 +1,24 @@
+package overload;
+
+class Add {
+
+	int sum(int a, int b) {
+		return a + b;
+	}
+
+	int sum(int a, int b, int c) {
+		return a + b + c;
+	}
+
+}
+
+public class Main {
+	public static void main(String[] args) {
+
+		Add a = new Add();
+		System.out.println(a.sum(10, 20));
+		System.out.println(a.sum(10, 20, 30));
+
+	}
+
+}
